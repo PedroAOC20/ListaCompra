dc2b82a9d7ea86ed654689d1e9eeda1b05a47da4
PedroAOC20 <pedrodeveloperpyc@gmail.com>
2024-10-05 10:47:42 -0300

    Criado a classe de Usuario

---
+++ app/src/main/java/com/example/lista_compra/usuario/modelo/Usuario.kt
@@ -0,0 +1,28 @@
+package com.example.lista_compra.usuario.modelo
+
+data class Usuario(
+    val nome: String,
+    val email: String,
+    val senha: String
+)
+
+object UsuarioRepositorio {
+    private val usuarios = mutableListOf<Usuario>()
+
+    fun cadastrar(usuario: Usuario): Boolean {
+        // Verifica se o email já está cadastrado
+        if (usuarios.any { it.email == usuario.email }) {
+            return false // Email já cadastrado
+        }
+        usuarios.add(usuario)
+        return true // Cadastro bem-sucedido
+    }
+
+    fun listarUsuarios(): List<Usuario> {
+        return usuarios
+    }
+
+    fun autenticar(email: String, senha: String): Usuario? {
+        return usuarios.find { it.email == email && it.senha == senha }
+    }
+}
