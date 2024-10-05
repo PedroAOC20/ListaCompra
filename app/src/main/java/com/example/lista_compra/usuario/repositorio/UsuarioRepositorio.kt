6aa86a96729c037dba20afc5362c62140f4dbdce
PedroAOC20 <pedrodeveloperpyc@gmail.com>
2024-10-05 10:52:23 -0300

    Criação do diretório UsuarioRepositorio com um usuário admin iniciando aplicação.

---
+++ app/src/main/java/com/example/lista_compra/usuario/repositorio/UsuarioRepositorio.kt
@@ -0,0 +1,26 @@
+package com.example.lista_compra.usuario.repositorio
+
+import com.example.lista_compra.usuario.modelo.Usuario
+import java.util.concurrent.CopyOnWriteArrayList
+
+object UsuarioRepositorio {
+    private val usuarios = CopyOnWriteArrayList<Usuario>()
+
+    init {
+        // inicia aplicação com usuario admin criado.
+        usuarios.add(Usuario("admin", "admin@admin.com", "admin123"))
+    }
+
+    fun cadastrar(usuario: Usuario): Boolean {
+        // Verifica se o email já está cadastrado
+        if (usuarios.any { it.email == usuario.email }) {
+            return false
+        }
+        usuarios.add(usuario)
+        return true
+    }
+
+    fun autenticar(email: String, senha: String): Usuario? {
+        return usuarios.find { it.email == email && it.senha == senha }
+    }
+}
