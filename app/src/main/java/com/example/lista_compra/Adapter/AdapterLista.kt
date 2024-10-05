8778c0f5e3cd7d341279ba4fd5066e42189d83c4
FernandoYaginuma <fernandoyaginuma@gmail.com>
2024-10-05 19:19:57 -0300

    feat: AdapterLista

---
+++ app/src/main/java/com/example/lista_compra/Adapter/AdapterLista.kt
@@ -0,0 +1,35 @@
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
+import com.example.myapplication.model.Listas
+
+class AdapterLista(private val context: Context, val list_listas: ArrayList<Listas>): RecyclerView.Adapter<AdapterLista.ListasViewHolder>() {
+
+    inner class ListasViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val foto = itemView.findViewById<ImageView>(R.id.foto)
+        val nome = itemView.findViewById<TextView>(R.id.nomeProduto)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListasViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val l = LayoutInflater.from(context).inflate(R.layout.lista_list,parent,false)
+        return ListasViewHolder(l)
+    }
+
+    override fun getItemCount(): Int {
+        return list_listas.size
+    }
+
+    override fun onBindViewHolder(holder: ListasViewHolder, position: Int) {
+        val newList = list_listas[position]
+        holder.foto.setImageURI(newList.foto)
+        holder.nome.text = newList.nome
+    }
+}
