a991e095191f545f62b0942a889dfbb04e6c1b48
PedroAOC20 <pedrodeveloperpyc@gmail.com>
2024-10-05 12:25:13 -0300

    Removido o objeto UsuarioRepositorio devido a duplicidade existente do arquivo UsuarioRepositorio.kt da pasta Repositorio.

--- app/src/main/java/com/example/lista_compra/usuario/modelo/Usuario.kt
+++ app/src/main/java/com/example/lista_compra/usuario/modelo/Usuario.kt
@@ -5,21 +5,3 @@ data class Usuario(
     val email: String,
     val senha: String
 )
-
-object UsuarioRepositorio {
-    private val usuarios = mutableListOf<Usuario>()
-
-    fun cadastrar(usuario: Usuario): Boolean {
-        // Verifica se o email já está cadastrado
-        if (usuarios.any { it.email == usuario.email }) {
-            return false // Email já cadastrado
-        }
-        // Adiciona o usuário à lista
-        usuarios.add(usuario)
-        return true
-    }
-
-    fun autenticar(email: String, senha: String): Usuario? {
-        return usuarios.find { it.email == email && it.senha == senha }
-    }
-}
